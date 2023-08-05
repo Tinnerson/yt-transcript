430ce47777865ecf6ff8f26c8933bd9b4b09ac50
류인호 <128089943+Tinnerson@users.noreply.github.com>
2023-08-05 11:45:16 -0600

    Update main.py
    Ok made it a LOT simpler, but only works for one video rn. Also made it output to txt file but idk if that might cause problems in the grand scheme of things... I will see

--- main.py
+++ main.py
@@ -1,83 +1,20 @@
 import speech_recognition as sr
-import moviepy.editor as mp
+from pydub import AudioSegment
 import os
-import ffmpy
-import time
-from io import BufferedReader
 
-import soundfile
-data, samplerate = soundfile.read('ENG_M.wav')
-soundfile.write('new.wav', data, samplerate, subtype='PCM_16')
+video = AudioSegment.from_file("video.mp4", format="mp4")
+audio = video.set_channels(1).set_frame_rate(16000).set_sample_width(2)
+audio.export("audio.wav", format="wav")
 
-def convert_audio(path: str = None,
-                  convert: str = 'wav',
-                  stream: BufferedReader = None) -> dict:
-    error_message = None
+r = sr.Recognizer()
 
-    try:
-        _audio = AudioSegment.from_file_using_temporary_files(stream) \
-            if stream else AudioSegment.from_file(path)
-    except FileNotFoundError as error:
-        error_message = 'Audio file was not found (convert audio)'
+with sr.AudioFile("audio.wav") as source:
+    audio_text = r.record(source)
 
-        logging.exception(error_message)
+text = r.recognize_google(audio_text, language='en-US')
 
-        return dict(error=error_message)
-    except Exception as error:
-        error_message = 'Unexpected error when open audio file (convert audio)'
+file_name = "transcription.txt"
 
-        logging.exception(error_message)
-
-        return dict(error=error_message)
-
-    audio_converted = _audio.export(format=convert)
-
-    audio_converted.seek(0)
-
-    return dict(
-        audio=_audio,
-        audio_converted=audio_converted,
-        error=error_message
-    )
-
-
-def audio(path: str = None,
-          stream: BufferedReader = None) -> dict:
-    recognizer = speech_recognition.Recognizer()
-
-    error_message = None
-
-    try:
-        _ = convert_audio(
-            stream=stream) if stream else convert_audio(path=path)
-
-        if _.get('error'):
-            error_message = _.get('error')
-
-            logging.exception(error_message)
-
-            return dict(error=error_message)
-
-        _audio = _.get('audio_converted')
-
-        with speech_recognition.AudioFile(_audio) as file:
-            _audio = recognizer.record(file)
-    except FileNotFoundError as error:
-        error_message = 'Audio file was not found (audio)'
-
-        logging.exception(error_message)
-
-        return dict(error=error_message)
-    except Exception as error:
-        error_message = 'Unexpected error when open audio file (audio)'
-
-        logging.exception(error_message)
-
-        return dict(error=error_message)
-
-    text_sphinx = recognizer.recognize_sphinx(_audio)
-
-    return dict(
-        text=text_sphinx,
-        error=error_message
-    )
+with open(file_name, "w") as file:
+    file.write(text)
+os.system(f"start {file_name}")
